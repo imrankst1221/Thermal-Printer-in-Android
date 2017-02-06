f435702f02802bcf4f338effcd5e1d8cba01655d
DESKTOP-MR9868R\Imran PC <imrankst1221@gmail.com>
2017-02-06 15:19:10 +0600

    initial connection.

---
+++ ThermalPrinter/app/src/main/java/nullpointer/thermal/printer/DeviceList.java
@@ -0,0 +1,285 @@
+package nullpointer.thermal.printer;
+
+/**
+ * Created by https://goo.gl/UAfmBd on 2/6/2017.
+ */
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
+
+import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+public class DeviceList extends ListActivity {
+
+    static public final int REQUEST_CONNECT_BT = 0*2300;
+
+    static private final int REQUEST_ENABLE_BT = 0*1000;
+
+    static private BluetoothAdapter mBluetoothAdapter = null;
+
+    static private ArrayAdapter<String> mArrayAdapter = null;
+
+    static private ArrayAdapter<BluetoothDevice> btDevices = null;
+
+    private static final UUID SPP_UUID = UUID
+            .fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
+    // UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    static private BluetoothSocket mbtSocket = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setTitle("Bluetooth Devices");
+
+        try {
+            if (initDevicesList() != 0) {
+                this.finish();
+                return;
+            }
+
+        } catch (Exception ex) {
+            this.finish();
+            return;
+        }
+
+        IntentFilter btIntentFilter = new IntentFilter(
+                BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mBTReceiver, btIntentFilter);
+    }
+
+    public static BluetoothSocket getSocket() {
+        return mbtSocket;
+    }
+
+    private void flushData() {
+        try {
+            if (mbtSocket != null) {
+                mbtSocket.close();
+                mbtSocket = null;
+            }
+
+            if (mBluetoothAdapter != null) {
+                mBluetoothAdapter.cancelDiscovery();
+            }
+
+            if (btDevices != null) {
+                btDevices.clear();
+                btDevices = null;
+            }
+
+            if (mArrayAdapter != null) {
+                mArrayAdapter.clear();
+                mArrayAdapter.notifyDataSetChanged();
+                mArrayAdapter.notifyDataSetInvalidated();
+                mArrayAdapter = null;
+            }
+
+            finalize();
+
+        } catch (Exception ex) {
+        } catch (Throwable e) {
+        }
+
+    }
+    private int initDevicesList() {
+
+        flushData();
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(),
+                    "Bluetooth not supported!!", Toast.LENGTH_LONG).show();
+            return -1;
+        }
+
+        if (mBluetoothAdapter.isDiscovering()) {
+            mBluetoothAdapter.cancelDiscovery();
+        }
+
+        mArrayAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_list_item_1);
+
+        setListAdapter(mArrayAdapter);
+
+        Intent enableBtIntent = new Intent(
+                BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        try {
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        } catch (Exception ex) {
+            return -2;
+        }
+
+        Toast.makeText(getApplicationContext(),
+                "Getting all available Bluetooth Devices", Toast.LENGTH_SHORT)
+                .show();
+
+        return 0;
+
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent intent) {
+        super.onActivityResult(reqCode, resultCode, intent);
+
+        switch (reqCode) {
+            case REQUEST_ENABLE_BT:
+
+                if (resultCode == RESULT_OK) {
+                    Set<BluetoothDevice> btDeviceList = mBluetoothAdapter
+                            .getBondedDevices();
+                    try {
+                        if (btDeviceList.size() > 0) {
+
+                            for (BluetoothDevice device : btDeviceList) {
+                                if (btDeviceList.contains(device) == false) {
+
+                                    btDevices.add(device);
+
+                                    mArrayAdapter.add(device.getName() + "\n"
+                                            + device.getAddress());
+                                    mArrayAdapter.notifyDataSetInvalidated();
+                                }
+                            }
+                        }
+                    } catch (Exception ex) {
+                    }
+                }
+
+                break;
+        }
+        mBluetoothAdapter.startDiscovery();
+
+    }
+
+    private final BroadcastReceiver mBTReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent
+                        .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                try {
+                    if (btDevices == null) {
+                        btDevices = new ArrayAdapter<BluetoothDevice>(
+                                getApplicationContext(), android.R.id.text1);
+                    }
+
+                    if (btDevices.getPosition(device) < 0) {
+                        btDevices.add(device);
+                        mArrayAdapter.add(device.getName() + "\n"
+                                + device.getAddress() + "\n" );
+                        mArrayAdapter.notifyDataSetInvalidated();
+                    }
+                } catch (Exception ex) {
+// ex.fillInStackTrace();
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onListItemClick(ListView l, View v, final int position,
+                                   long id) {
+        super.onListItemClick(l, v, position, id);
+
+        if (mBluetoothAdapter == null) {
+            return;
+        }
+
+        if (mBluetoothAdapter.isDiscovering()) {
+            mBluetoothAdapter.cancelDiscovery();
+        }
+
+        Toast.makeText(
+                getApplicationContext(),
+                "Connecting to " + btDevices.getItem(position).getName() + ","
+        + btDevices.getItem(position).getAddress(),
+                Toast.LENGTH_SHORT).show();
+
+        Thread connectThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    boolean gotuuid = btDevices.getItem(position)
+                            .fetchUuidsWithSdp();
+                    UUID uuid = btDevices.getItem(position).getUuids()[0]
+                            .getUuid();
+                    mbtSocket = btDevices.getItem(position)
+                            .createRfcommSocketToServiceRecord(uuid);
+
+                    mbtSocket.connect();
+                } catch (IOException ex) {
+                    runOnUiThread(socketErrorRunnable);
+                    try {
+                        mbtSocket.close();
+                    } catch (IOException e) {
+// e.printStackTrace();
+                    }
+                    mbtSocket = null;
+                    return;
+                } finally {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            finish();
+
+                        }
+                    });
+                }
+            }
+        });
+
+        connectThread.start();
+    }
+
+    private Runnable socketErrorRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            Toast.makeText(getApplicationContext(),
+                    "Cannot establish connection", Toast.LENGTH_SHORT).show();
+            mBluetoothAdapter.startDiscovery();
+
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(0, Menu.FIRST, Menu.NONE, "Refresh Scanning");
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case Menu.FIRST:
+                initDevicesList();
+                break;
+        }
+
+        return true;
+    }
+}
--- ThermalPrinter/app/src/main/java/nullpointer/thermal/printer/MainActivity.java
+++ ThermalPrinter/app/src/main/java/nullpointer/thermal/printer/MainActivity.java
@@ -1,13 +1,109 @@
 package nullpointer.thermal.printer;
 
-import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by https://goo.gl/UAfmBd on 2/6/2017.
+ */
+import android.app.Activity;
 import android.os.Bundle;
+import java.io.IOException;
+import java.io.OutputStream;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity{
+    EditText message;
+    Button printbtn;
 
+    byte FONT_TYPE;
+    private static BluetoothSocket btsocket;
+    private static OutputStream btoutputstream;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        message = (EditText)findViewById(R.id.message);
+        printbtn = (Button)findViewById(R.id.printButton);
+
+        printbtn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                connect();
+            }
+        });
+    }
+
+    protected void connect() {
+        if(btsocket == null){
+            Intent BTIntent = new Intent(getApplicationContext(), DeviceList.class);
+            this.startActivityForResult(BTIntent, DeviceList.REQUEST_CONNECT_BT);
+        }
+        else{
+            OutputStream opstream = null;
+            try {
+                opstream = btsocket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            btoutputstream = opstream;
+            print_bt();
+
+        }
+
+    }
+    private void print_bt() {
+        try {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            btoutputstream = btsocket.getOutputStream();
+
+            byte[] printformat = { 0x1B, 0*21, FONT_TYPE };
+            btoutputstream.write(printformat);
+            String msg = message.getText().toString();
+            btoutputstream.write(msg.getBytes());
+            btoutputstream.write(0x0D);
+            btoutputstream.write(0x0D);
+            btoutputstream.write(0x0D);
+            btoutputstream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            if(btsocket!= null){
+                btoutputstream.close();
+                btsocket.close();
+                btsocket = null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            btsocket = DeviceList.getSocket();
+            if(btsocket != null){
+                print_bt();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
